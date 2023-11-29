7f5fc95fbe6818d04792ac92c08b50b2591db64b
YoungBin Lee <enebin777@gmail.com>
2023-11-29 20:20:27 +0900

    Initial Commit

---
+++ SwiftNetworkRequestExamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftNetworkRequestExamples
+//
+//  Created by YoungBin Lee on 11/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftNetworkRequestExamples/SwiftNetworkRequestExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkRequestExamplesApp.swift
+//  SwiftNetworkRequestExamples
+//
+//  Created by YoungBin Lee on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetworkRequestExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
